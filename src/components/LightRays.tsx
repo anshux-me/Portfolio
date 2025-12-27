19ddd767cad99ed0d394dea258be4ed9d81e76aa
anshukr384 <anshukr384@gmail.com>
2025-12-27 11:53:39 +0530

    4th iteration with lightray effect

--- src/components/LightRays.tsx
+++ src/components/LightRays.tsx
@@ -448,12 +448,12 @@ void main() {
     <div
       ref={containerRef}
       style={{
-        width: '100%',
-        height: '100%',
+        width: '100vw',
+        height: '100vh',
         pointerEvents: 'none',
-        zIndex: 1,
+        zIndex: 0,
         overflow: 'hidden',
-        position: 'absolute',
+        position: 'fixed',
         top: 0,
         left: 0
       }}
